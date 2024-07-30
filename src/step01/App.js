b4bb919d37f394e33160b62486f54aba365bc0aa
casey casey <caseycasey859@gmail.com>
2024-07-30 11:29:10 +0900

    step03

---
+++ src/step01/App.js
@@ -0,0 +1,39 @@
+// 1.로딩페이지
+import './App.css';
+import {useState, useEffect} from 'react';
+
+function App() {
+  //상태변수를 설정
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadCounter, setLoadCounter] = useState(0);
+  useEffect(
+    ()=>{
+      function IncreaseCounter(){
+        let _counter = loadCounter;
+        _counter = _counter + 1;
+        if(_counter >10){
+          setIsLoading(false);
+          clearInterval(loadTimer);
+        }
+    
+        setLoadCounter(
+          _counter
+        );
+    
+      }
+      const loadTimer =setInterval(
+        IncreaseCounter,1000
+      );
+      return ()=>{ clearInterval(loadTimer);}
+      
+
+    },[loadCounter]
+  )
+  return (
+    <div >
+      {isLoading? `loading... ${loadCounter}`: 'Loaded'}
+    </div>
+  );
+}
+
+export default App;
